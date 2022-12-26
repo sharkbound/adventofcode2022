52fe41cc5c5cbfb78e03647edd74a605e3029d10
sharkbound <laptopblaster@gmail.com>
2022-12-26 16:40:22 -0600

    day 9 done

--- days/day_9/day_9_part_2.py
+++ days/day_9/day_9_part_2.py
@@ -7,6 +7,10 @@ from day import Day
 import numpy as np
 
 
+def new_pos(x, y) -> np.ndarray:
+    return np.array([x, y])
+
+
 @dataclass
 class Move:
     dir: str
@@ -15,21 +19,29 @@ class Move:
 
 @dataclass
 class Node:
+    id: int
     pos: np.ndarray
     parent: Optional[Node] = None
     child: Optional[Node] = None
 
     @classmethod
     def create_chain(cls, length):
-        default_pos = np.array([0, 0])
-        head = Node(default_pos)
+        id_ = 0
+        head = Node(id_, new_pos(0, 0))
         tail = head
         for i in range(length - 1):
-            child = Node(default_pos.copy())
+            id_ += 1
+            child = Node(id_, new_pos(0, 0))
             tail.child = child
             child.parent = tail
             tail = child
         return head, tail
+
+    def __iter__(self):
+        node = self
+        while node is not None:
+            yield node
+            node = node.child
 
 
 X = 0
@@ -51,7 +63,7 @@ class Day9Part2(Day):
         return ''
 
     def parse_input(self):
-        data = self.read_sample_file_lines(0)
+        data = self.input_text_lines
         # data = self.read_sample_file_lines(0)
         ret = []
         for line in data:
@@ -71,18 +83,23 @@ class Day9Part2(Day):
 
         return x_change, y_change
 
+    def display_nodes(self, nodes: Node):
+        positions = {}
+        for node in nodes:
+            positions.setdefault(tuple(node.pos), []).append(node.id)
+        print(positions)
+
     def is_child_out_of_range_of_parent(self, child, parent):
         return abs(child[X] - parent[X]) > 1 or abs(child[Y] - parent[Y]) > 1
 
     def process_movement(self, head: Node):
         node = head.child
         while node:
-            if self.is_child_out_of_range_of_parent(node.pos, node.parent.pos):
+            if not self.is_child_out_of_range_of_parent(node.pos, node.parent.pos):
                 break
 
             move = self.get_movement_for_nodes(node.parent.pos, node.pos)
-            node.pos[X] += move[X]
-            node.pos[Y] += move[Y]
+            node.pos += move
             node = node.child
 
     def solve(self):
@@ -115,5 +132,6 @@ class Day9Part2(Day):
                 head.pos[Y] += offset[Y]
                 self.process_movement(head)
                 add_tail_pos()
+                # self.display_nodes(head)
 
         self.print_answer(len(tail_positions))
