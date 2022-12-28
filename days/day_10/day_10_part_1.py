b4097399c5a33a288b433cba5b1546c1eca95a48
sharkbound <laptopblaster@gmail.com>
2022-12-27 18:22:05 -0600

    day 10 start

---
+++ days/day_10/day_10_part_1.py
@@ -0,0 +1,19 @@
+
+from day import Day
+import re
+import numpy as np
+import utils
+
+
+class Day10Part1(Day):
+    day = 10
+    part = 1
+
+    def get_sample_input(self):
+        return ''
+
+    def parse_input(self):
+        return ''
+
+    def solve(self):
+        data = self.parse_input()
