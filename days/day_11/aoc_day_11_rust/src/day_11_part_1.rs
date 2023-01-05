a72746ca7f31fa5a0a609a2526be64b574167731
sharkbound <laptopblaster@gmail.com>
2023-01-04 22:44:35 -0600

    almost correct output for day 11 part 1

--- days/day_11/aoc_day_11_rust/src/day_11_part_1.rs
+++ days/day_11/aoc_day_11_rust/src/day_11_part_1.rs
@@ -1,47 +1,74 @@
-use std::fmt::{Debug, Display, Formatter};
+use std::collections::VecDeque;
+use std::fmt::{Debug};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::ops::Index;
 use std::path::Path;
+
+use rustutils::{str_split};
+
+/*
+Operation shows how your worry level changes as that monkey inspects an item.
+
+After each monkey inspects an item but before it tests your worry level,
+    your relief that the monkey's inspection didn't damage the item causes your worry level...
+    to be divided by three and rounded down to the nearest integer.
+
+The monkeys take turns inspecting and throwing items.
+On a single monkey's turn, it inspects and throws all of the items it is holding one at a time and in the order listed.
+*/
 
 pub struct Day11Part1<'a> {
     input_path: &'a Path,
 }
 
+#[derive(Debug)]
 enum OperationSign {
-    PLUS,
-    MUL,
+    PLUS(SubstituteValue, SubstituteValue),
+    MUL(SubstituteValue, SubstituteValue),
 }
 
 impl OperationSign {
-    pub fn apply_to(&self, value1: u32, value2: u32) -> u32 {
+    pub fn apply(&self, item: u32) -> u32 {
         match self {
-            OperationSign::PLUS => value1 + value2,
-            OperationSign::MUL => value1 * value2,
-        }
-    }
-
-    pub fn name(&self) -> &'static str {
-        match self {
-            OperationSign::PLUS => "PLUS",
-            OperationSign::MUL => "MUL",
+            OperationSign::PLUS(sub1, sub2) => sub1.resolve(item) + sub2.resolve(item),
+            OperationSign::MUL(sub1, sub2) => sub1.resolve(item) * sub2.resolve(item),
         }
     }
 }
 
+#[derive(Debug)]
+enum SubstituteValue {
+    ITEM,
+    LITERAL(u32),
+}
 
-struct Monkey<'a> {
+impl SubstituteValue {
+    fn resolve(&self, item: u32) -> u32 {
+        match &self {
+            SubstituteValue::ITEM => item,
+            SubstituteValue::LITERAL(v) => *v,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
     id: u32,
-    items: Vec<u32>,
+    items: VecDeque<u32>,
     operation: OperationSign,
-    test: &'a dyn Fn(u32) -> bool,
-    test_pass_target: u32,
-    test_fail_target: u32,
+    div_test: u32,
+    true_target: usize,
+    false_target: usize,
+    inspect_count: u32,
 }
 
-impl<'a> Debug for Monkey<'a> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, r#"Monkey {{ id: {} }}"#, self.id)
+impl Monkey {
+    fn add_item(&mut self, item: u32) {
+        self.items.push_back(item);
+    }
+
+    fn next_item(&mut self) -> Option<u32> {
+        self.items.pop_front()
     }
 }
 
@@ -74,42 +101,104 @@ impl<'a> Day11Part1<'a> {
                         current_group.push(val);
                     }
                 }
-                Err(_) => { break; }
+                Err(_) => {
+                    println!("EERRRRROOORRR");
+                    break;
+                }
             }
         };
+
+        if !current_group.is_empty() {
+            groups.push(current_group);
+        }
 
         groups.into_iter().map(|group| self.parse_monkey(group)).collect()
     }
 
-    fn separate_string(&self, line: &'a String, sep: &str) -> Vec<&str> {
-        line.split(sep).collect::<Vec<_>>()
+    fn parse_substitute(string: &str) -> SubstituteValue {
+        match string {
+            "old" => SubstituteValue::ITEM,
+            int => {
+                SubstituteValue::LITERAL(int.parse::<u32>()
+                    .expect(format!(r#"invalid u32 when parsing substitute value "{}""#, string).as_str()))
+            }
+        }
     }
 
-    fn parse_monkey_operation(&self, line: &String) -> OperationSign {
-        let parts = self.separate_string(line, " ");
+    fn parse_monkey_operation(&self, line: &str) -> OperationSign {
+        let parts = str_split!(line);
         let calc_parts = parts[parts.iter().position(|x| *x == "=").unwrap() + 1..].iter().collect::<Vec<_>>();
         match *calc_parts[1] {
-            "+" => OperationSign::PLUS,
-            "*" => OperationSign::MUL,
+            "+" => OperationSign::PLUS(Self::parse_substitute(calc_parts[0]), Self::parse_substitute(calc_parts[2])),
+            "*" => OperationSign::MUL(Self::parse_substitute(calc_parts[0]), Self::parse_substitute(calc_parts[2])),
             symbol => { panic!(r#"unexpected operation "{}""#, symbol) }
         }
+    }
+
+    fn parse_u32_at_end_of_line(line: &str) -> u32 {
+        line.trim()[line.trim().rfind(" ").unwrap() + 1..].parse::<u32>().unwrap()
+    }
+
+    fn parse_usize_at_end_of_line(line: &str) -> usize {
+        line.trim()[line.trim().rfind(" ").unwrap() + 1..].parse::<usize>().unwrap()
     }
 
     fn parse_monkey(&self, group: Vec<String>) -> Monkey {
         Monkey {
             id: group[0].chars().into_iter().filter(|x| x.is_digit(10)).collect::<String>().parse::<u32>().unwrap(),
             items: group[1][group[1].find(":").unwrap() + 1..].trim().split(',').map(|x| x.trim().parse::<u32>().unwrap()).collect(),
-            operation: self.parse_monkey_operation(&group[2]),
-            test: &|_| true,
-            test_pass_target: 0,
-            test_fail_target: 0,
+            operation: self.parse_monkey_operation(group[2].as_str()),
+            div_test: Self::parse_u32_at_end_of_line(group[3].as_str()),
+            true_target: Self::parse_usize_at_end_of_line(group[4].as_str()),
+            false_target: Self::parse_usize_at_end_of_line(group[5].as_str()),
+            inspect_count: 0,
+        }
+    }
+
+    fn do_round_for_single_monkey(monkey: &mut Monkey) -> Vec<(usize, u32)> {
+        let mut dests = vec![];
+        while let Some(item) = monkey.next_item() {
+            let div_result = monkey.operation.apply(item) / 3;
+
+            if div_result / monkey.div_test == 0 {
+                dests.push((monkey.true_target, div_result));
+            } else {
+                dests.push((monkey.false_target, div_result));
+            }
+        }
+        dests
+    }
+
+    fn do_round(monkeys: &mut Vec<Monkey>) {
+        for i in 0..monkeys.len() {
+            let monkey = monkeys.get_mut(i).unwrap();
+            for (dest, val) in Self::do_round_for_single_monkey(monkey) {
+                monkeys.get_mut(dest).unwrap().add_item(val);
+            }
         }
     }
 
     pub fn solve(&self) {
-        let monkeys = self.parse();
+        let mut monkeys = self.parse();
+        for _ in 0..20 {
+            Self::do_round(&mut monkeys);
+        }
+
+        /*
+            these values after round 20 are wrong:
+                0: [21, 13, 12, 6]
+                1: [30, 22, 26, 29, 17, 17]
+                2: []
+                3: []
+
+            should be:
+                Monkey 0: 10, 12, 14, 26, 34
+                Monkey 1: 245, 93, 53, 199, 115
+                Monkey 2:
+                Monkey 3:
+         */
         for m in monkeys {
-            println!("id: {} operation: {}", m.id, m.operation.name())
+            println!("{}: {:?}", m.id, m.items);
         }
     }
 }
---
+++ rustutils/src/string_macros.rs
@@ -0,0 +1,57 @@
+#[macro_export]
+/// splits a [str] or [String] into a [Vec] of [&str]
+///
+/// # Usage:
+/// ## spliting by whitespace:
+/// ```
+/// use rustutils::str_split;
+/// let string = "hello, world";
+/// // ["hello,", "world"]
+/// let split = str_split!(string);
+/// ```
+/// ## spliting by custom delimiter:
+/// ```
+/// use rustutils::str_split;
+/// let string = "hello, world";
+/// // ["hello", " world"]
+/// let split = str_split!(string, ",");
+/// ```
+macro_rules! str_split {
+    ($str:expr) => {
+        $str.split_whitespace().collect::<Vec<&str>>()
+    };
+
+    ($str:expr, $sep:expr) => {
+        $str.split($sep).collect::<Vec<&str>>()
+    };
+}
+
+#[macro_export]
+/// splits a [str] or [String] into a [Vec] of [String]
+///
+/// # Usage:
+/// ## spliting by whitespace:
+/// ```
+/// use rustutils::string_split;
+/// let string = "hello, world";
+/// // ["hello,", "world"]
+/// let split = string_split!(string);
+/// ```
+/// ## spliting by custom delimiter:
+/// ```
+/// use rustutils::string_split;
+/// let string = "hello, world";
+/// // ["hello", " world"]
+/// let split = string_split!(string, ",");
+/// ```
+macro_rules! string_split {
+    // string_split!(items);
+    ($string:expr) => {
+        $string.split_whitespace().map(|s|s.to_string()).collect::<Vec<String>>()
+    };
+
+    // string_split!(items, " ");
+    ($string:expr, $sep:expr) => {
+        $string.split($sep).map(|s|s.to_string()).collect::<Vec<String>>()
+    };
+}
