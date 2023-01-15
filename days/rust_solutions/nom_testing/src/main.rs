9d98d47b71163448aa86f97505efaf87358fcb85
sharkbound <laptopblaster@gmail.com>
2023-01-14 23:28:57 -0600

    understanding nom better

--- days/rust_solutions/nom_testing/src/main.rs
+++ days/rust_solutions/nom_testing/src/main.rs
@@ -1,19 +1,53 @@
-use nom::{character, IResult};
-use nom::bytes::complete::{tag};
-use nom::multi::separated_list1;
+use nom::{IResult};
+use nom::bytes::complete::{is_not, tag, take_till};
+use nom::bytes::streaming::{is_a};
+use nom::character::complete::{newline};
+use nom::character::{is_alphabetic, is_alphanumeric};
+
+
+static TO_PARSE: &'static str = "\
+Items
+    milk
+    eggs
+    crackers
+";
 
 
 fn main() {
     let ints = "1 -2 10 86 1996 1857 1000";
-    dbg!(parse_ints(ints).ok());
+    println!("{:?}", match parse(TO_PARSE) {
+        Ok(value) => {
+            println!("{:?}", value);
+        }
+        Err(e) => {
+            match e {
+                nom::Err::Incomplete(v) => { println!("{:?}", v) }
+                nom::Err::Error(e) => { println!("code: {:?}", e.code) }
+                nom::Err::Failure(f) => { println!("{:?}", f.code) }
+            }
+        }
+    });
 }
 
+fn not_whitespace(input: &str) -> IResult<&str, &str> {
+    is_not(" \r\n\t")(input)
+}
 
-fn parse_ints(input: &str) -> IResult<&str, Vec<i32>> {
-    //                            input V, V return type
-    fn parse(input: &str) -> IResult<&str, i32> {
-        character::complete::i32(input)
+fn parse(input: &str) -> IResult<&str, Vec<&str>> {
+    let (input, _) = tag("Items")(input)?;
+    let (input, _) = newline(input)?;
+    let (mut input, _) = is_a(" \t\n")(input)?;
+    let mut items = vec![];
+    let mut _trash = "";
+    loop {
+        (input, _trash) = take_till(|x: char| x.is_alphanumeric())(input)?;
+        input = match not_whitespace(input) {
+            Ok((rest, item)) => {
+                items.push(item);
+                rest
+            }
+            Err(_) => return Ok((input, items)),
+        };
     }
-    // separated_list1(tag(" "), |x| character::complete::i32(x))(input)
-    separated_list1(tag(" "), parse)(input)
+    Ok(("", items))
 }
