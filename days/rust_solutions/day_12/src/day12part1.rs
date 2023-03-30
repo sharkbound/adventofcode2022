3e63b5d45717afd1e97304f6e6a95dee27513fe7
sharkbound <laptopblaster@gmail.com>
2023-03-29 21:22:44 -0500

    working more on day 12 in rust

--- days/rust_solutions/day_12/src/day12part1.rs
+++ days/rust_solutions/day_12/src/day12part1.rs
@@ -16,10 +16,6 @@ impl Point {
         Point { x, y }
     }
 
-    fn at_edge(&self, bounds: [usize; 2]) -> bool {
-        self.x == 0 || self.x == bounds[1] || self.y == 0 || self.y == bounds[0]
-    }
-
     fn as_usize_array_yx(&self) -> [usize; 2] {
         [self.y, self.x]
     }
@@ -42,19 +38,31 @@ static ALLOWED_OFFSETS: [Offset; 4] = [
     Offset { x: 0, y: 1 },
 ];
 
-static START_INT: i32 = -1;
-static END_INT: i32 = -2;
+static START_HEIGHT: i32 = -1;
+static END_HEIGHT: i32 = -2;
+static START_CHAR: char = 'S';
+static END_CHAR: char = 'E';
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct PointData {
     pub point: Point,
     pub height: i32,
 }
 
+impl PointData {
+    fn as_char(&self) -> char {
+        match self.height {
+            i if i == START_HEIGHT => 'S',
+            i if i == END_HEIGHT => 'E',
+            i => char::from_digit(i as u32 + 'a'.to_digit(10).unwrap(), 10).unwrap(),
+        }
+    }
+}
+
 fn char_heightmap_to_i32_height(heightmap: &Array2<char>) -> Array2<i32> {
     let i32_heightmap = heightmap.map(|x| match *x {
-        'S' => START_INT,
-        'E' => END_INT,
+        'S' => START_HEIGHT,
+        'E' => END_HEIGHT,
         val => val as i32 - 'a' as i32,
     });
     i32_heightmap
@@ -62,20 +70,16 @@ fn char_heightmap_to_i32_height(heightmap: &Array2<char>) -> Array2<i32> {
 
 pub struct Day12part1 {
     file: PathBuf,
-    heightmap: Array2<char>,
-    i32_heightmap: Array2<i32>,
+    raw_heightmap: Array2<char>,
+    heightmap: Array2<i32>,
 }
 
-enum BoundsCheck {
-    Enabled,
-    Disabled,
-}
 impl Day12part1 {
     pub fn new(input_path: PathBuf) -> Self {
         Self {
             file: input_path,
-            heightmap: Array2::from_elem([0, 0], ' '),
-            i32_heightmap: Array2::from_elem([0, 0], 0),
+            raw_heightmap: Array2::from_elem([0, 0], ' '),
+            heightmap: Array2::from_elem([0, 0], 0),
         }
     }
 
@@ -98,21 +102,32 @@ impl Day12part1 {
     }
 
     fn _find_char(&self, find_char: char) -> Option<PointData> {
-        self.heightmap.indexed_iter().find_map(|((y, x), c)| {
-            if *c == find_char {
-                Some(PointData {
-                    point: Point::new(x, y),
-                    height: self.i32_heightmap[[y, x]],
-                })
-            } else {
-                None
-            }
-        })
+        self.raw_heightmap
+            .indexed_iter()
+            .map(|((y, x), c)| (Point::new(x, y), *c))
+            .find_map(|(point, c)| {
+                if c == find_char {
+                    Some(self.get_point_data(point))
+                } else {
+                    None
+                }
+            })
     }
 
-    fn path_find(&self, start: &Point, should_stop: impl Fn(&Point) -> bool) -> Vec<Vec<Point>> {
-        let mut path = vec![*start];
+    fn path_find(&self, start: &Point, /*should_stop: impl Fn(&Point) -> bool*/) -> Vec<Vec<Point>> {
+        let mut path = vec![self.get_point_data(*start)];
         let mut paths = Vec::new();
+        loop {
+            let current_data = path.last().unwrap();
+            let neighbors = self.neighbors(current_data.point);
+            match neighbors.iter().find(|data| data.height == END_HEIGHT) {
+                None => {}
+                Some(data) => {
+                    path.push(*data)
+                }
+            }
+            todo!();
+        }
         paths
     }
 
@@ -122,27 +137,32 @@ impl Day12part1 {
             _ => return None,
         };
 
-        let (bounds_y, bounds_x) = self.i32_heightmap.shape().map_to(|x| (x[0], x[1]));
+        let (bounds_y, bounds_x) = self.heightmap.shape().map_to(|x| (x[0], x[1]));
         if point.x >= bounds_x || point.y >= bounds_y {
             return None;
         }
-
         Some(point)
     }
 
+    fn get_point_data(&self, point: Point) -> PointData {
+        PointData {
+            point,
+            height: self.heightmap[point.as_usize_array_yx()],
+        }
+    }
+
     fn neighbors(&self, point: Point) -> Vec<PointData> {
-        let current_height = self.i32_heightmap[point.as_usize_array_yx()];
+        let current_height = self.heightmap[point.as_usize_array_yx()];
         let mut neighbors = Vec::new();
         for offset in ALLOWED_OFFSETS.iter() {
             let [x, y] = point.map_to(|Point { x, y }| [x as i32 + offset.x, y as i32 + offset.y]);
-
             match self.create_point(x, y) {
                 Some(point) => {
-                    let height = self.i32_heightmap[point.as_usize_array_yx()];
+                    let height = self.heightmap[point.as_usize_array_yx()];
                     if (height - current_height).abs() > 1 {
                         continue;
                     }
-                    neighbors.push(PointData { height, point });
+                    neighbors.push(self.get_point_data(point));
                 }
                 None => continue,
             }
@@ -151,9 +171,9 @@ impl Day12part1 {
     }
 
     pub fn solve(&mut self) {
-        self.heightmap = self.parse();
-        self.i32_heightmap = char_heightmap_to_i32_height(&self.heightmap);
-        self.i32_heightmap.debug();
+        self.raw_heightmap = self.parse();
+        self.heightmap = char_heightmap_to_i32_height(&self.raw_heightmap);
+        self.heightmap.debug();
         let (start, end) = (self._find_char('S').unwrap(), self._find_char('E').unwrap());
         println!("start: {:?}, end: {:?}", start, end);
     }
